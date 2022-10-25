d7a672bfe7594d53bd93a846e60b3469e15b822a
kave haj <kave.haj@snapp.cab>
2022-10-26 01:42:40 +0330

    added handler

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
-
+	"fmt"
+	"log"
+	"net/http"
 )
 
-func main(){
-	
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server on Port 8080 \n")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
